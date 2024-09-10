bc01ed9af0ae4f3e32716d73e8a7169f53188372
Misael Gaytan <gaytanmisael@gmail.com>
2024-09-09 22:43:01 -0700

    misael:Started Components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
+
+import icon from "astro-icon";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  integrations: [tailwind({ applyBaseStyles: false }), icon(), alpinejs()]
+})
